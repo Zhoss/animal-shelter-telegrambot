a71ab7d5b8ef6324b081fed5865f208cd6f72b5c
Zhoss <joss.90@mail.ru>
2023-03-23 16:37:11 +1000

    Initial commit

---
+++ src/main/java/pro/sky/teamwork/animalsheltertelegrambot/AnimalShelterTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.teamwork.animalsheltertelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterTelegrambotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelterTelegrambotApplication.class, args);
+    }
+
+}
