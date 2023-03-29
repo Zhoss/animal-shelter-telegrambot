31e75e3738f25d738057b504e6182746377a683b
KlochkoVN <89vklochko89@gmail.com>
2023-03-29 20:55:33 +0300

    Выдает бесконечный цикл при запуске (Убрал обвертку, все равно бесконечный цикл)

--- src/main/java/pro/sky/teamwork/animalsheltertelegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/teamwork/animalsheltertelegrambot/listener/TelegramBotUpdatesListener.java
@@ -5,6 +5,7 @@ import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import pro.sky.teamwork.animalsheltertelegrambot.commands.Commands;
 import javax.annotation.PostConstruct;
 import java.util.List;
 
+import static pro.sky.teamwork.animalsheltertelegrambot.commands.Commands.GREET_MSG;
 
 
 @Service
@@ -21,9 +23,11 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     private final TelegramBot telegramBot;
+    private final Commands commands;
 
     public TelegramBotUpdatesListener(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
+        this.commands = new Commands();
     }
 
     @PostConstruct
@@ -33,17 +37,23 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> updates) {
-        updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            Message message = update.message();
-            try {message.text().equals(Commands.START_COMMAND);
-                logger.info("Command's been received: " + Commands.START_COMMAND);
 
-            } catch (Exception e){
+            updates.forEach(update -> {
+                logger.info("Processing update: {}", update);
+                Message message = update.message();
+                if (message.text().equals(Commands.START_COMMAND)) {
+                    logger.info("Command's been received: " + Commands.START_COMMAND);
+                    sendMessage(showChatId(message), GREET_MSG);
+                }
+            });
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
 
-            }
-        });
-        return UpdatesListener.CONFIRMED_UPDATES_ALL;
-
+    }
+    private Long showChatId(Message message) {
+        return message.chat().id();
+    }
+    private void sendMessage(Long chatId, String messageContent) {
+        SendMessage sendMessage = new SendMessage(chatId, messageContent);
+        telegramBot.execute(sendMessage);
     }
 }
