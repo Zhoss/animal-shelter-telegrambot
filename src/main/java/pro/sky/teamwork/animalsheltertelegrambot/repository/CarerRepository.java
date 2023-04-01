c5121d13aa41eb1f060d78bf5fe0786a6faa5105
Zhoss <joss.90@mail.ru>
2023-04-01 23:12:26 +1000

    Изменения №2 от 01.04.2023 - добавлен интерфейс CarerRepository для взаимодействия с БД; - добавлен класс CarerService для добавления опекуна в БД; - в TelegramBotConfiguration добавлен список команд (SetMyCommands); - в TelegramBotUpdatesListener добавлены константы команд, заинжектен CarerService; - код метода process() обернут в try-catch; - добавлены обработки команд в зависимости от присланного сообщения или нажатой кнопки InlineKeyboard; - добавлены методы для создания клавиатур основного меню и меню с информацией о приюте.

---
+++ src/main/java/pro/sky/teamwork/animalsheltertelegrambot/repository/CarerRepository.java
@@ -0,0 +1,8 @@
+package pro.sky.teamwork.animalsheltertelegrambot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pro.sky.teamwork.animalsheltertelegrambot.model.Carer;
+
+public interface CarerRepository extends JpaRepository<Carer, Long> {
+
+}
