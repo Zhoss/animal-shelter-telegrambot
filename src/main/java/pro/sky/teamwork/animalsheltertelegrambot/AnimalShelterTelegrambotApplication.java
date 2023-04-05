304a01bc80bc2f0ecf88eb04ed72c54bb1382f4d
Zhoss <joss.90@mail.ru>
2023-04-05 18:17:25 +1000

    Revert "- добавлены зависимости в pom.xml; - подключен liquibase, создан change log, создан файл со скриптами/SQL-запросами, созданы запросы на создание таблиц БД; - созданы модели/сущности опекунов, собак, соглашений(договоров), ежедневных отчетов. Таблицы и сущность команд для бота (который через /) решил не создавать, т.к. делать запросы в БД просто для получения команды для сравнения с входящим сообщением нерационально - считаю, лучше хранить данные команды в коллекции и через метод искать и возвращать нужную. - добавил пакеты и классы configuration и listener из шаблона для телеграмбота из предыдущей курсовой; - добавил аннотацию @EnableScheduling над запускающим классом (Application), внес изменение в имя запускающего класса для соблюдения camelCase (AnimalShelterTelegrambotApplication -> AnimalShelterTelegramBotApplication)"
    This reverts commit 2c75713d

--- src/main/java/pro/sky/teamwork/animalsheltertelegrambot/AnimalShelterTelegrambotApplication.java
+++ src/main/java/pro/sky/teamwork/animalsheltertelegrambot/AnimalShelterTelegrambotApplication.java
@@ -2,14 +2,12 @@ package pro.sky.teamwork.animalsheltertelegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-public class AnimalShelterTelegramBotApplication {
+public class AnimalShelterTelegrambotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AnimalShelterTelegramBotApplication.class, args);
+        SpringApplication.run(AnimalShelterTelegrambotApplication.class, args);
     }
 
 }
